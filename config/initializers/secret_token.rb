5932f69647ac66de94b9e5fa9f483385b60c67e4
Leon Dewey <ldewey@Leons-MacBook-Pro.local>
2013-05-09 12:18:03 +1200

    WSF-4727 Added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Customerconsole::Application.config.secret_key_base = '6b0e92e63ddd10e1c65fa064554d0f5aaad1ac17d19448f40b19a206c961f37fe233cb80fb6b923a4425523e0ef1ae04b41aadf553bd83cb50480ea34179b7b0'

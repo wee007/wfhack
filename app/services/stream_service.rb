db0080fac22db3469261050cb278a6de5be8e486
Cameron Walsh <cwalsh2@au.westfield.com>
2013-06-04 18:41:44 +1000

    Request stream service from its own host

--- app/services/stream_service.rb
+++ app/services/stream_service.rb
@@ -6,7 +6,7 @@ class StreamService
     end
 
     def request_uri(centre)
-      uri = URI("#{ENV['HOST']}/api/stream/master/stream.json")
+      uri = URI("http://stream-service.#{Rails.env}.dbg.westfield.com/api/stream/master/stream.json")
       uri.query = {centre: centre}.to_query
       uri
     end

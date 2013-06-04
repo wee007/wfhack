a8b237cb4eb4670ad778d93937b9860655070b0d
Cameron Walsh <cwalsh2@au.westfield.com>
2013-06-04 18:53:46 +1000

    Fix specs

--- spec/services/stream_service_spec.rb
+++ spec/services/stream_service_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe StreamService do
   it 'generates a request uri per centre' do
-    expect(StreamService.request_uri('burwood').to_s).to eq 'http://stream-service.test.dbg.westfield.com/api/stream/master/stream.json?centre=burwood'
+    expect(StreamService.request_uri('burwood').to_s).to eq "http://stream-service.#{Rails.env}.dbg.westfield.com/api/stream/master/stream.json?centre=burwood"
   end
 end

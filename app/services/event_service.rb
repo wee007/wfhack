23f5106cb2c1896a954b5c37e4fcf583566cd401
Leon Dewey <ldewey@au.westfield.com>
2013-06-05 17:12:32 +1200

    Refactor per feedback.

--- app/services/event_service.rb
+++ app/services/event_service.rb
@@ -9,9 +9,9 @@ class EventService
       if options.is_a?(Fixnum) || options.is_a?(String)
         URI("http://event-service.#{Rails.env}.dbg.westfield.com/api/event/master/events/#{options}.json")
       elsif options.present?
-        query = Addressable::URI.new
-        query.query_values = options
-        URI("http://event-service.#{Rails.env}.dbg.westfield.com/api/event/master/events.json?#{query.query}")
+        uri = URI("http://event-service.#{Rails.env}.dbg.westfield.com/api/event/master/events.json")
+        uri.query = options.to_query
+        uri
       else
         URI("http://event-service.#{Rails.env}.dbg.westfield.com/api/event/master/events.json")
       end

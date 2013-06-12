a9d3fa0b9fc3f5ee1a51a962587bc18a5e617051
Cameron Walsh <cwalsh2@au.westfield.com>
2013-06-12 12:37:54 +1000

    More, shorter methods

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,7 +2,8 @@ module ProductsHelper
 
   # TODO: Delete 'type' and use sub_category instead once product search api
   # facets on sub_category correctly. Alternatively, make it appear anyway.
-  def category_facet_tags facets
+  def category_facet_tags search_results
+    facets = search_results.facets
     super_cat_tag = hidden_field_tag :super_cat, params[:super_cat] if params[:super_cat].present?
     category_tag = hidden_field_tag :category, params[:category] if params[:category].present?
     sub_category_tag = hidden_field_tag :sub_category, params[:sub_category] if params[:sub_category].present?
@@ -11,14 +12,24 @@ module ProductsHelper
     [super_cat_tag, category_tag, sub_category_tag, category_facet_tag].compact.join.html_safe
   end
 
+  def chosen_select_tag(tag_name, values, selected, display_name: nil, class_name: nil, **opts)
+    select_tag tag_name, options_for_select(values, selected),
+      opts.merge(class: ["chzn-select",class_name].compact.join(" "),
+      'data-placeholder' => display_name || "#{tag_name}".titleize.pluralize)
+  end
+
+  def facet_values(facet_name, facets)
+    facets.send(facet_name)[:values].map do |facet|
+      [facet.name.titleize, facet.code]
+    end
+  end
+
   def facet_tag(facet_name, facets, display_name: nil, multiple: false)
     return nil if facets.send(facet_name).nil?
     tag_name = multiple ? "#{facet_name}[]" : facet_name
-    select_tag tag_name,
-      options_for_select(facets.send(facet_name)[:values].map do |facet|
-        [facet.name.titleize, facet.code]
-      end, params[facet_name]),
-      include_blank: true, class: "chzn-select facet-#{facet_name}", multiple: multiple,
-      'data-placeholder' => display_name || "#{facet_name}".titleize.pluralize
+    values = facet_values(facet_name, facets)
+    chosen_select_tag tag_name, values, params[facet_name],
+      display_name: display_name || "#{facet_name}".titleize.pluralize,
+      multiple: multiple, class_name: "facet-#{facet_name}"
   end
 end

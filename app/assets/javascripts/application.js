2036c1daf4e4c4bf63e4365c447a63f5d6c4c23f
Ben Schwarz <ben@germanforblack.com>
2013-06-18 09:53:20 +1000

    Copied over assets from frontend_framework. These will need updating after the PR is merged.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,188 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-//= require chosen-jquery
-//= require jquery.ui.slider
+// require jquery
+// require jquery_ujs
+// require turbolinks
+// require_tree .
+
+// Pull in author & 3rd party files
+
+//= require ./author/jquery.toggle-click
+//= require ./vendor/polyfills/matchMedia
+//= require ./author/jquery.toggle-menu
+//= require ./author/jquery.toggle-search
+//= require ./vendor/enquire
+//= require ./vendor/jquery.twitter-bootstrap-dropdown
+
+
+/* --HELPERS-- */
+
+/*
+ * Avoid `console` errors in browsers that lack a console
+ * @credit  http://html5boilerplate.com/
+ */
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+
+/* --GLOBAL VARS-- */
+
+var
+// Google map, if we want access 'map' outside of initialize()
+//map,
+// Get viewport width
+viewport = document.body.clientWidth,
+// Breakpoints (BP) (these need to match the CSS breakpoints)
+BPpalmAlt = 640,
+BPpalm    = 'all and (max-width: 40em)',    // 640px
+BPlap     = 'all and (min-width: 40.0625em)', // 641px
+// Is palm sized viewport
+isPalmSizedScreen = (viewport <= BPpalmAlt),
+// HTML element
+htmlElement = $('html'),
+// `tel:` links
+telLinks = $('a[href^="tel:"]'),
+// Is touch device
+isTouch = ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch,
+// Is iOS device
+isiOS = navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i),
+// Is old IE
+isOldIE = htmlElement.hasClass('old-ie'),
+// Is JS enabled
+isJS = htmlElement.hasClass('js'),
+// Android V2- detection
+androidV2 = false,
+androidStart = navigator.userAgent.indexOf("Android "),
+androidVer = navigator.userAgent.substr(androidStart+8,1),
+// Is there SVG support
+isSVG = document.implementation.hasFeature('http://www.w3.org/TR/SVG11/feature#Image', '1.1');
+
+// Enable caching for conditionally loading scripts via `getScript`
+$.getScript = function(url, callback, cache) {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: callback,
+        dataType: "script",
+        cache: cache
+    });
+};
+
+/* --FIRE ON DOM READY-- */
+$(function() {
+
+    /* --Conditionally load polyfills-- */
+
+    // HTML5 input placeholder attr
+    if (!('placeholder' in $('<input>')[0])) {
+        $.getScript('//cdnjs.cloudflare.com/ajax/libs/jquery-placeholder/2.0.7/jquery.placeholder.min.js', function() {
+            $('input, textarea').placeholder();
+        }, true);
+    }
+
+
+    /* --Run plugins-- */
+
+    // Run 'Toggle menu for palm sized viewports' plugin
+    $('.js-menu-toggle').toggleMenu();
+
+    // Run 'Toggle search for palm sized viewports plugin' plugin
+    $('.js-search-toggle').toggleSearch();
+
+
+    /* --DOM lookups-- */
+
+
+    /* --Tid bits-- */
+
+    // Apply hook for iOS devices
+    if (isiOS) {
+        htmlElement.addClass('ios');
+    }
+
+    // Apply hook for SVG support/non-support
+    if (isSVG) {
+        htmlElement.addClass('svg');
+    } else {
+        htmlElement.addClass('no-svg');
+    }
+
+    // Replace SVG logo with PNG version for non-supporting browsers (Non JS support: http://www.noupe.com/tutorial/svg-clickable-71346.html - last technique)
+    if (!isSVG) {
+        $('img[src*="svg"]').attr('src', function() {
+            return $(this).attr('src').replace('.svg', '.png');
+        });
+    }
+
+    // Add hook to `html` if Android V2
+    if (androidVer <= 2) {
+        androidV2 = true;
+    }
+    if (androidV2) {
+        htmlElement.addClass('androidv2');
+    }
+
+
+    /* --Do stuff on `$(window).resize` via Enquire library-- */
+    enquire.register(BPlap, {
+
+        deferSetup: true,
+
+        setup: function() {
+
+            // Conditionally load scripts
+            /*$.getScript('/Cheetah/js/modules/conditionally-loaded.js', function() {
+                $('.js-menu-accessible-ddown').accessibleDropDownMenu();
+            }, true);*/
+
+        },
+
+        // Not palm size viewport
+        match: function() {
+
+            // Test
+            //$('body').css('background', 'gold');
+
+            // Disable focus and click events for `tel` links
+            telLinks.attr('tabindex', '-1');
+            telLinks.click(function(e) {
+                e.preventDefault();
+            });
+
+        },
+
+        // Palm size viewport
+        unmatch: function() {
+
+            // Test
+            //$('body').css('background', 'hotpink');
+
+            // Enable focus and click events for `tel` links
+            telLinks.removeAttr('tabindex');
+            telLinks.unbind('click');
+
+        }
+
+    }, true).listen();
+
+});
---
+++ app/assets/javascripts/author/jquery.toggle-search.js
@@ -0,0 +1,44 @@
+/*
+ * Toggle search for palm sized viewports plugin
+ * @desc        used to toggle the global header search for palm sized viewports
+ * @initialise  $('.js-search-toggle').toggleSearch();
+ */
+$.fn.toggleSearch = function() {
+
+    // Set variables
+    var toogleBtn = $('.js-search-btn-toggle'),
+        toggleBtnTxt = toogleBtn.children('.hide-visually'),
+        search = $(this),
+        searchContainer = $(this).find('.search-mini'),
+        menu = $('.js-menu-toggle'),
+        header = $('.header');
+
+    // Toggle on click event
+    $(toogleBtn).toggleClick(
+        // Expanded state
+        function() {
+            search.toggleClass('is-expanded');
+            $(this).toggleClass('is-expanded');
+            toggleBtnTxt.html('Close site search');
+            // Remove the header menu
+            if (menu.hasClass('is-expanded')) {
+                menu.removeClass('is-expanded');
+            }
+            // Set focus to the search form so screen readers and keyboard users can jump to it as the search form is not directly after the search toggle button
+            searchContainer.attr('tabindex', -1).focus();
+            // Reset header fixed positiong which is changed via the menu toggle plugin (when menu is open fixed positioning is changed to `static`)
+            header.css('position', 'fixed');
+        },
+        // Collapsed state
+        function() {
+            search.toggleClass('is-expanded');
+            $(this).toggleClass('is-expanded');
+            toggleBtnTxt.html('Open site search');
+            // Remove the header menu
+            if (menu.hasClass('is-expanded')) {
+                menu.removeClass('is-expanded');
+            }
+            // Reset header fixed positiong which is changed via the menu toggle plugin (when menu is open fixed positioning is changed to `static`)
+            header.css('position', 'fixed');
+    })
+}

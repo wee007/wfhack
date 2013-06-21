036a3fb8a641d6a6bc2177af6a22ef3d7b1159f6
Eric Wee <ewee@au.westfield.com>
2013-06-21 12:17:18 +1000

    Add trading hours link and add temp inline css on trading hours page for showcasing

--- app/controllers/trading_hours_controller.rb
+++ app/controllers/trading_hours_controller.rb
@@ -1,7 +1,7 @@
 class TradingHoursController < ApplicationController
 
   def index
-    @centres = CentreService.build CentreService.fetch params[ :centre_id ]
+    @centre = @centres = CentreService.build CentreService.fetch params[ :centre_id ]
   end
 
 end

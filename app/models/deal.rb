eda722b51e2620b026acdf92ce37972440312248
Craig Smith <craigm.smith@au.westfield.com>
2013-06-24 15:36:47 +1000

    Added the store name to the image alt text.

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,9 +1,21 @@
 class Deal < OpenStruct
 
   def retailer_logo_url
-    store_service = StoreService.fetch(deal_stores.first.store_service_id)
-    store = StoreService.build store_service
     store._links.logo.href
+  end
+
+  def store_name
+    store.name
+  end
+
+private
+  def store
+    @store ||= get_store
+  end
+
+  def get_store
+    store_service = StoreService.fetch(deal_stores.first.store_service_id)
+    StoreService.build store_service
   end
 
 end
--- spec/models/deal_spec.rb
+++ spec/models/deal_spec.rb
@@ -1,6 +1,28 @@
 require "spec_helper"
 
 describe Deal do
+
+  context "When asking for the retailer name and logo" do
+    it "should only query the store service once" do
+      mock_deal_store = stub('store_service_id' => 123, "name" => "Wittner", "_links" => stub({'logo' => stub({'href' => "http://example.com/wittner.jpg"})}))
+      StoreService.should_receive(:fetch).with(123).once.and_return("STORE_SERVICE")
+      StoreService.should_receive(:build).with("STORE_SERVICE").once.and_return(mock_deal_store)
+
+      deal = Deal.new(deal_stores:[mock_deal_store])
+      deal.retailer_logo_url
+      deal.store_name
+    end
+  end
+
+  it "should get the store name" do
+    mock_deal_store = stub('store_service_id' => 123, "name" => "Wittner")
+    StoreService.should_receive(:fetch).with(123).once.and_return("STORE_SERVICE")
+    StoreService.should_receive(:build).with("STORE_SERVICE").once.and_return(mock_deal_store)
+
+    deal = Deal.new(deal_stores:[mock_deal_store])
+    expect(deal.store_name).to eql("Wittner")
+  end
+
   it "should get the retailer logo url" do
     mock_deal_store = stub('store_service_id' => 123, "_links" => stub({'logo' => stub({'href' => "http://example.com/wittner.jpg"})}))
 

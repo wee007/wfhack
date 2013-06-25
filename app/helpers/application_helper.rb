3070cee967d0fe9ff1988b33c8137fdecfcba478
Matthew Wratt <matt.wratt@trineo.co.nz>
2013-06-25 16:57:36 +1200

    adds default placeholder image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
   end
 
   def responsive_image_tag(options = {})
-    src = options.delete(:placeholder)
+    src = options.delete(:placeholder) || default_placeholder_image
     options[:data] = { :src => options.delete(:normal) }
     if options.has_key? :retina
       options[:data][:'src-retina'] = options.delete(:retina)
@@ -21,6 +21,15 @@ module ApplicationHelper
     image_tag(src, options) + content_tag(:noscript) do
       image_tag options[:data][:src]
     end
+  end
+
+  def default_placeholder_image
+    protocol = 'http://'
+    image_service = "image-service.#{Rails.env}.dbg.westfield.com"
+    customer_console = "customer-console.#{Rails.env}.dbg.westfield.com"
+    image = "assets/logo-tile.png"
+    attr = "size=60x60&pad=true&background=ffffff"
+    protocol + image_service + '/transform?url=' + protocol + customer_console + '/' + image + '&' + attr
   end
 
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -4,9 +4,14 @@ describe ApplicationHelper do
 
   context "#responsive_image_tag" do
 
+    let(:default_placeholder) { 'http://image-service.test.dbg.westfield.com/transform?url=http://customer-console.test.dbg.westfield.com/assets/logo-tile.png&size=60x60&pad=true&background=ffffff' }
     let(:placeholder) { 'placeholder_image.png' }
     let(:normal) { 'normal_image.png' }
     let(:retina) { 'retina_image.png' }
+
+    it "returns the image with default placeholder" do
+      responsive_image_tag(normal: normal).should == image_tag(default_placeholder, :'data-src' => normal) + content_tag(:noscript) { image_tag normal }
+    end
     
     it "returns an image tag" do
       responsive_image_tag(placeholder: placeholder, normal: normal).should == image_tag(placeholder, data: {src: normal}) + content_tag(:noscript) { image_tag normal }

6f7334460f8edf751b87b838ba3923380e85d977
Leon Dewey <ldewey@au.westfield.com>
2013-06-25 15:29:35 -0700

    "sub layout", for detail_view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,4 +27,9 @@ module ApplicationHelper
     asset_path 'placeholder.png'
   end
 
+  def parent_layout(layout)
+    @view_flow.set(:layout,output_buffer)
+    self.output_buffer = render(:file => "layouts/#{layout}")
+  end
+
 end

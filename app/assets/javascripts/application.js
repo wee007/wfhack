08915609890704675af15d545a8a6df205466dae
Ben Schwarz <ben@germanforblack.com>
2013-07-01 11:26:42 +1000

    Move add bower references to items previously handled in rubygems

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,19 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-// require jquery_ujs
-// require turbolinks
-// require_tree .
 
 // Pull in author & 3rd party files
 
@@ -124,6 +108,8 @@ $(function() {
   // Run 'Calculate the height of the fixed header' plugin
   $('.header').calculateHeaderHeight();
 
+// External dependencies, handled via Bower.
+// See: https://gist.github.com/benschwarz/5874031/
 
   /* --DOM lookups-- */
 
@@ -186,6 +172,8 @@ $(function() {
 
       // Run 'Calculate the height of the fixed header' plugin
       $('.header').calculateHeaderHeight();
+//= require jquery
+//= require jquery-ujs/src/rails.js
 
     },
 

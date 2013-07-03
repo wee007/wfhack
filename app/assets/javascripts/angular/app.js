6acc0d1f1d50428eed094ae60469ac0b483f4b56
Ben Schwarz <ben@germanforblack.com>
2013-07-04 09:45:02 +1000

    Add base app. We'll be using CORS, so enable it globally.

---
+++ app/assets/javascripts/angular/app.js
@@ -0,0 +1,6 @@
+var app = angular.module( 'Westfield', [] );
+
+app.config( ['$httpProvider', function( $httpProvider ) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);

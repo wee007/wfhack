d37476bafc9988a8f9f573f77de9e0ed19bdc04a
Chris Pearce <CPearce@au.westfield.com>
2013-07-04 17:57:30 +1000

    cleaning up comments

--- app/assets/javascripts/support/svg.js
+++ app/assets/javascripts/support/svg.js
@@ -1,5 +1,5 @@
-// Replace SVG logo with PNG version for non-supporting browsers (Non JS support: http://www.noupe.com/tutorial/svg-clickable-71346.html - last technique)
 if (!Modernizr.svg) {
+  // Replace SVG logo with PNG version for non-supporting browsers (Non JS support: http://www.noupe.com/tutorial/svg-clickable-71346.html - last technique)
   $('img[src*="svg"]').attr('src', function() {
     return $(this).attr('src').replace('.svg', '.png');
   });

b9ca5b73c7252ae632c5ca220297e5fc8b17f21c
Ben Schwarz <ben@germanforblack.com>
2013-07-04 14:28:47 +1000

    Todo notes

--- app/assets/javascripts/angular/controllers/product_browser.js
+++ app/assets/javascripts/angular/controllers/product_browser.js
@@ -10,6 +10,19 @@
     $scope.selectedRetailers = [];
     $scope.selectedBrands = [];
 
+    // TODO
+    // available_filters
+    // super_cat
+    // category
+    // sub category is multi-value
+
+    // router
+    // /bondijunction/products?retailer[]=supre
+    // search by multi-centre - centre[]
+    // centre[]=all
+
+    // supré vs supre in filtering down results
+
     // Multi-value facets must be sent back using the original
     // name of the model. eg. selectedBrands should become brands
     // Use this method instead of filterSearch for multi-value fields

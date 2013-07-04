32d50052af2854bb5d0d4fa0f7492d9cf72a67f9
Cameron Walsh <cwalsh2@au.westfield.com>
2013-07-05 09:36:49 +1000

    ProductService - work with show url too

--- app/services/product_service.rb
+++ app/services/product_service.rb
@@ -3,12 +3,16 @@ class ProductService
     include ApiClientRequests
     def build(json)
       results = json.respond_to?(:body) ? json.body : json
-      facets = build_facets(results)
-      products = results.delete :results
-      pagination = build_pagination(results)
-      sort_options = results.delete(:display_options)[:sort_options]
-      Hashie::Mash.new results.merge(products: products, facets: facets,
-        sort_options: sort_options).merge(pagination)
+      if results.has_key? :facets
+        facets = build_facets(results)
+        products = results.delete :results
+        pagination = build_pagination(results)
+        sort_options = results.delete(:display_options)[:sort_options]
+        Hashie::Mash.new results.merge(products: products, facets: facets,
+          sort_options: sort_options).merge(pagination)
+      else
+        results
+      end
     end
 
   private
@@ -28,12 +32,28 @@ class ProductService
     end
 
     def request_uri(options)
-      options.delete_if {|o| %w(action controller).include?(o.to_s)}
+      options.delete :controller
+      action = options.delete :action
+      options.reject! {|k,v|v.blank? || v.is_a?(Array) && v.all?(&:blank?) }
+      if action == 'show'
+        show_uri(options)
+      else
+        search_uri(options)
+      end
+    end
+
+    def show_uri(options)
+      retailer_code = options.delete :retailer_code
+      sku = options.delete :sku
+      URI("#{ServiceHelper.uri_for('product')}/retail-chains/#{retailer_code}/products/#{sku}.json")
+    end
+
+    def search_uri(options)
       search_opts = ({page:1, rows: 50}.merge options).with_indifferent_access
       centre = search_opts.delete :centre_id
       search_opts[:centre] ||= centre
       uri = URI("#{ServiceHelper.uri_for('product')}/products/search.json")
-      uri.query = search_opts.reject{|k,v|v.blank? || v.is_a?(Array) && v.all?(&:blank?) }.to_query
+      uri.query = search_opts.to_query
       uri
     end
   end

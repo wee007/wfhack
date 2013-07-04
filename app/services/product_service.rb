73449c3b8ce86d6a9055e938d9a71c2b67aa61c8
Cameron Walsh <cwalsh2@au.westfield.com>
2013-07-04 17:39:15 +1000

    Product Service url has changed, no need to use AppConfig now.

--- app/services/product_service.rb
+++ app/services/product_service.rb
@@ -32,7 +32,7 @@ class ProductService
       search_opts = ({page:1, rows: 50}.merge options).with_indifferent_access
       centre = search_opts.delete :centre_id
       search_opts[:centre] ||= centre
-      uri = URI("#{AppConfig.product_service_url}/products/search.json")
+      uri = URI("#{ServiceHelper.uri_for('product')}/products/search.json")
       uri.query = search_opts.reject{|k,v|v.blank? || v.is_a?(Array) && v.all?(&:blank?) }.to_query
       uri
     end

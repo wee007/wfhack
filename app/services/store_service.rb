fbd98a3564e7c6f94896718d9acc91a714aa8718
Cameron Walsh <cwalsh2@au.westfield.com>
2013-07-08 17:06:31 +1000

    WSF-4924 Build stores from json - should make the Store model do this

--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -2,8 +2,16 @@ class StoreService
   class << self
     include ApiClientRequests
 
-    def request_uri(options=nil)
+    def build(resp)
+      json = resp.respond_to?(:body) ? resp.body : resp
+      if json.is_a? Array
+        json.map{|store| Store.new(store) }
+      else
+        Store.new(json)
+      end
+    end
 
+    def request_uri(options=nil)
       if options.is_a?(Fixnum) || options.is_a?(String)
         URI("#{ServiceHelper.uri_for('store')}/stores/#{options}.json")
       elsif options.present?
@@ -13,7 +21,6 @@ class StoreService
       else
         URI("#{ServiceHelper.uri_for('store')}/stores.json")
       end
-
     end
   end
 end

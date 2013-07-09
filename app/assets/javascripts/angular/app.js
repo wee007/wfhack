e6409391fff97e89710d913960372ea0f43fab59
Ben Schwarz <ben@germanforblack.com>
2013-07-09 19:05:42 +1000

    Added routes to pass the centre name

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,6 +1,17 @@
-var app = angular.module( 'Westfield', [] );
+var app = angular.module( 'Westfield', ['ngRoute'] );
 
-app.config( ['$httpProvider', function( $httpProvider ) {
+app.config(function ( $httpProvider, $routeProvider, $locationProvider ) {
+  // Allow CORS
   $httpProvider.defaults.useXDomain = true;
   delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}]);
+
+  // Routes
+  $routeProvider.when( '/:centre/products', {
+    controller: 'BrowseController',
+    templateUrl: 'productBrowseTemplate'
+  });
+
+  // Use pushState
+  $locationProvider.hashPrefix( '!' );
+  $locationProvider.html5Mode( true );
+});

32ca928dcc9429f8bf3c6caef3d0a12aa2476bfb
Craig Smith <craigm.smith@au.westfield.com>
2013-07-10 14:53:27 +1000

    Removing unused code.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,14 +12,4 @@ class MoviesController < ApplicationController
     @movies = MovieService.build movies
   end
 
-  def show
-    centre, movies = nil
-    Service::API.in_parallel do
-      centre = CentreService.fetch params[:centre_id]
-      movie = MovieService.fetch params[:id]
-    end
-    @centre = CentreService.build centre
-    @movie = MovieService.build movie
-  end
-
 end

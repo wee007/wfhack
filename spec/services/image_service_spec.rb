d595dad116aa6b6af6b9eb1ac66767a77bd09cfe
Leon Dewey <ldewey@au.westfield.com>
2013-07-24 10:19:10 +1200

    Build fix

--- spec/services/image_service_spec.rb
+++ spec/services/image_service_spec.rb
@@ -29,19 +29,19 @@ describe ImageService do
 
   it 'generates retina image url' do
     expect(subject.transform ref: 'ref', width: 100, height: 100, retina: true).to eq(
-      "#{subject.url}/transform?quality=25&ref=ref&size=200x200"
+      "#{subject.url}/transform?ref=ref&size=200x200"
     )
   end
 
   it 'generates retina image url, with only width passed' do
     expect(subject.transform ref: 'ref', width: 400, retina: true).to eq(
-      "#{subject.url}/transform?quality=25&ref=ref&size=800x800"
+      "#{subject.url}/transform?ref=ref&size=800x800"
     )
   end
 
   it 'generates retina image url, with only height passed' do
     expect(subject.transform ref: 'ref', height: 400, retina: true).to eq(
-      "#{subject.url}/transform?quality=25&ref=ref&size=800x800"
+      "#{subject.url}/transform?ref=ref&size=800x800"
     )
   end
 

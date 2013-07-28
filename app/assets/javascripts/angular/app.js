fdef5fcdf3e638c3ca861ce9e5fadafd7e1c76fb
Ben Schwarz <ben@germanforblack.com>
2013-07-28 17:08:15 +1000

    Use hash bang

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -4,5 +4,5 @@ app.config(function ( $httpProvider, $locationProvider ) {
   // Tell rails that we're using XMLHttpRequests
   $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-  $locationProvider.html5Mode(true);
+  $locationProvider.html5Mode(true).hashPrefix( '!' );
 });

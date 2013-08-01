2bcc5eecce26cd11a371164a83be94f911fd6d57
Cameron Walsh <cwalsh2@au.westfield.com>
2013-08-01 10:09:14 +1000

    Fix more minification issues

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -1,8 +1,8 @@
 var app = angular.module( 'Westfield', ['ngMobile', 'ngSanitize'] );
 
-app.config(function ( $httpProvider, $locationProvider ) {
+app.config(['$httpProvider', '$locationProvider', function ( $httpProvider, $locationProvider ) {
   // Tell rails that we're using XMLHttpRequests
   $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
   $locationProvider.html5Mode(true).hashPrefix( '!' );
-});
+}]);

6c3044148168ba08a3db6c173c42c94bea65264a
Cameron Walsh <cwalsh2@au.westfield.com>
2013-08-02 16:45:16 +1000

    Pass through error codes if not in the range 200-299

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def render_404
-    raise ActionController::RoutingError.new('Not Found')
+    handle_error(NullObject.new :status => 404)
   end
+
+  def handle_error(null_object=nil)
+    null_object ||= NullObject.new
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/#{null_object.status}", :layout => false, :status => null_object.status }
+      format.xml  { head null_object.status }
+      format.json { head null_object.status }
+      format.any  { head null_object.status }
+    end
+  end
+
 end

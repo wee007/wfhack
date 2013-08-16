a531d6f7e89dd19bb75e0407197466445a157af8
Ben Schwarz <ben@germanforblack.com>
2013-08-16 10:33:32 +1000

    Remove nextPage method

--- app/assets/javascripts/angular/controllers/products_controller.js
+++ app/assets/javascripts/angular/controllers/products_controller.js
@@ -7,12 +7,5 @@
         $window.initPlugin.isotope();
       }, 500);
     });
-
-    $scope.nextPage = function () {
-      // If we're not already in the process of loading
-      if ( Products.isLoading === false ) {
-        Products.appendNextPage();
-      }
-    };
   }]);
 }( angular.module( 'Westfield' ) ) );

e6071b110b61030d03bc15838981d93811b4eb8a
Craig Smith <craigm.smith@au.westfield.com>
2013-08-27 16:57:47 +1000

    The movie service json has changed structure.
    Updating tests and code to handle it.

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -5,7 +5,7 @@ describe "Movies" do
   describe "When asking for movies for a centre with no cinema" do
     it "should return a 404" do
       VCR.use_cassette('sydney_movies') do
-        get centre_movies_path(centre_id: 'sydney', date: '17-07-2013')
+        get centre_movies_path(centre_id: 'sydney', date: '29-08-2013')
         expect(response.code).to eq "404"
       end
     end
@@ -15,13 +15,13 @@ describe "Movies" do
     describe "When there are movies" do
       before(:each) do
         VCR.use_cassette('bondijunction_movies') do
-          get centre_movies_path(centre_id: 'bondijunction', date: '15-07-2013')
+          get centre_movies_path(centre_id: 'bondijunction', date: '27-08-2013')
         end
       end
       it "should display the movie titles for Bondi Junction" do
         assert_select "#movies" do
-          assert_select ".title", "The Great Gatsby"
-          assert_select ".title", "Man of Steel"
+          assert_select ".title", "The Wolverine"
+          assert_select ".title", "Elysium"
         end
       end
       it "should display store info for the cinema at bondijunction" do
@@ -35,15 +35,14 @@ describe "Movies" do
 end
 
 describe "Movie" do
-  describe "When viewing Superman movie" do
+  describe "When viewing Wolverine movie" do
     before(:each) do
-      VCR.use_cassette('superman_movie') do
-        get centre_movie_path(centre_id: 'bondijunction', id: 557, date: '16-07-2013')
+      VCR.use_cassette('wolverine_movie') do
+        get centre_movie_path(centre_id: 'bondijunction', id: 54, date: '28-08-2013')
       end
     end
     it "display the details for that movie" do
-      assert_select "h1", "Man of Steel"
-      assert_select "p", "A young boy learns that he has extraordinary powers and is not of this Earth."
+      assert_select ".title", "The Wolverine"
     end
   end
 end

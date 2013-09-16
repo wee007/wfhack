f4fdb453a3c74b80d545d27533b89f6684dc6c4d
Craig Smith <craigm.smith@au.westfield.com>
2013-09-16 18:04:33 +1000

    Added link to Ts and Cs for parking.

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,4 +2,9 @@ class Parking < Hashie::Mash
   def rates
     @rates ||= parking_rates.map{|v| ParkingRate.new(v) }
   end
+
+  def terms_and_conditions_url
+    "http://res.cloudinary.com/wlabs/image/upload/#{pdf_ref}.pdf"
+  end
+
 end

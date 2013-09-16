81f306aea205b7ef0516da5d34797f6899e67a37
Craig Smith <craigm.smith@au.westfield.com>
2013-09-16 19:42:37 +1000

    Using proper cloudinary helper

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -2,9 +2,4 @@ class Parking < Hashie::Mash
   def rates
     @rates ||= parking_rates.map{|v| ParkingRate.new(v) }
   end
-
-  def terms_and_conditions_url
-    "http://res.cloudinary.com/wlabs/image/upload/#{pdf_ref}.pdf"
-  end
-
 end

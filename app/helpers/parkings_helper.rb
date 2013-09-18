fa9f49a3c6296d6e6a3411a4de3a1ae79ac307a9
Craig Smith <craigm.smith@au.westfield.com>
2013-09-18 10:01:18 +1000

    When max daily rates are 0 or empty, don't say free. Say nothing or say -.

--- app/helpers/parkings_helper.rb
+++ app/helpers/parkings_helper.rb
@@ -1,7 +1,7 @@
 module ParkingsHelper
   
-  def display_parking_rate(rate)
-    return "FREE" unless rate.to_f > 0
+  def display_parking_rate(rate, message = "FREE")
+    return message unless rate.to_f > 0
     "$#{rate}"
   end
   

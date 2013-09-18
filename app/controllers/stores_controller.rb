72e4373d16d4ce66f770935073e2ac81ada8a8e6
Matthew Wratt <matt.wratt@trineo.co.nz>
2013-09-18 16:52:30 +1200

    moves store model creation to service

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -7,11 +7,7 @@ class StoresController < ApplicationController
       store = StoreService.fetch centre: params[:centre_id], per_page: 1000
     end
     @centre = CentreService.build centre
-    stores = StoreService.build(store).map do |store_attrs| 
-      store = Store.new(store_attrs)
-      store.centre = @centre
-      store
-    end
+    stores = StoreService.build(store, centre: @centre)
     @stores = stores.group_by(&:first_letter)
     gon.push centre: @centre, stores: stores.map(&:to_gon)
     meta.push title: "#{@centre.short_name} stores"
@@ -24,8 +20,7 @@ class StoresController < ApplicationController
       store = StoreService.fetch params[:id]
     end
     @centre = CentreService.build centre
-    @store = Store.new StoreService.build store
-    @store.centre = @centre
+    @store = StoreService.build(store, centre: @centre)
     gon.push centre: @centre, stores: [@store.to_gon]
     meta.push title: "#{@store.name} at #{@centre.short_name}"
   end

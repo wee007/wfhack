fdfb7cda7b028d59ae435d1a346660bdb63d9962
Leon Dewey <ldewey@au.westfield.com>
2013-09-22 09:53:00 +1200

    Adding national product routes.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,15 +5,15 @@ class ProductsController < ApplicationController
   def index
     centre, nearby_centres, products = nil
     Service::API.in_parallel do
-      centre = CentreService.fetch params[:centre_id]
+      centre = CentreService.fetch params[:centre_id] if params[:centre_id]
       nearby_centres = CentreService.fetch nil, near_to: params[:centre_id]
       products = ProductService.fetch params.merge({rows: 15})
     end
-    @centre = CentreService.build centre
+    @centre = CentreService.build centre if params[:centre_id]
     @nearby_centres = CentreService.build nearby_centres
     @search = ProductService.build products
 
-    meta.push title: "#{@centre.short_name} Products"
+    meta.push title: [@centre.try(:short_name), "Products"].flatten.join(" ")
 
     if @search.is_a? NullObject
       handle_error(@search)
@@ -36,17 +36,23 @@ class ProductsController < ApplicationController
   def show
     centre, product, stores = nil
     Service::API.in_parallel do
-      centre = CentreService.fetch params[:centre_id]
+      centre = CentreService.fetch params[:centre_id] if params[:centre_id]
       product = ProductService.fetch params.dup
       stores = StoreService.fetch retailer_code: params[:retailer_code], per_page: 1000
     end
-    @centre = CentreService.build centre
+
     @product = ProductService.build product
     handle_error(@product) if @product.is_a? NullObject
+
     @stores = StoreService.build(stores)
     centre_ids = @stores.map(&:centre_id).uniq
     @centres = centre_ids.present? ? CentreService.find(:all, centre_id: centre_ids, near_to: params[:centre_id]) : []
-    @centre_stores = @stores.select {|store| store['centre_id'] == @centre['code']}
+
+    if params[:centre_id]
+      @centre = CentreService.build centre
+      @centre_stores = @stores.select {|store| store['centre_id'] == @centre['code']}
+    end
+
     gon.push centre: @centre, stores: @centre_stores
     meta.push @product.meta
   end

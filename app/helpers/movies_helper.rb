c39a59e01073e3684391952eddfa27ca171625ab
Craig Smith <craigm.smith@au.westfield.com>
2013-10-02 14:58:01 +1000

    Tidied up the movies helper a little.

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -2,11 +2,9 @@ module MoviesHelper
 
   def days(last_date, centre_id, selected_date = nil, movie_id = nil)
     selected_date = Time.now.strftime("%d-%m-%Y") if selected_date.nil?
-    today = Time.now
-    last_date = Time.parse(last_date)
 
     days = []
-    ((last_date - today)/60/60/24).ceil.times do |i|
+    number_of_days_until(last_date).times do |i|
       text = i.days.from_now.localtime.strftime("%a %-d %b")
       text = "Today" if i == 0
       text = "Tomorrow" if i == 1
@@ -19,6 +17,10 @@ module MoviesHelper
       end
     end
     days
+  end
+
+  def number_of_days_until(date_string)
+    (Date.parse(date_string) - Date.today).ceil
   end
 
 end

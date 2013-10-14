63f4d0dc1aa29da1263068e187ba3d8f416ca31e
Ben Schwarz <ben@germanforblack.com>
2013-10-15 09:57:04 +1100

    Don't run requests in parallel because it'll end up tripping over itself when it makes 70 requests at once.
    * Added logging messages
    * Run an api request per centre to get the store list rather than relying on ruby Array#find_all

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -14,29 +14,26 @@ SitemapGenerator::Sitemap.create do
   stores_uri = URI("#{ServiceHelper.uri_for('store')}/stores.json")
   product_search_url = URI("#{ServiceHelper.uri_for('product')}/products/search.json")
 
-  centres, stores = nil
-  Service::API.in_parallel do
-    centres = Service::API.get(centres_uri)
-    stores = Service::API.get(stores_uri)
-  end
+  centres = Service::API.get(centres_uri)
+  stores = Service::API.get(stores_uri)
 
   # Hit the product_search_url once to discover the amount of products / pages,
   # then loop over those.
-  product_count = Service::API.get(product_search_url, rows: 1).count
-  rows = 1000
+  product_count = Service::API.get(product_search_url, rows: 1)['count']
+  rows = 500
   pages = (product_count.to_f / rows.to_f).ceil
 
   pp "There are #{product_count} products, which will be collected #{rows} at a time"
 
   product_responses = []
-  Service::API.in_parallel do
-    (1..pages).to_a.each do |index|
-      pp "Getting product page #{index}"
+  (1..pages).to_a.each do |index|
+    pp "Getting product page #{index}"
       product_responses << Service::API.get(product_search_url, page: index, rows: rows)
-    end
   end
 
-  centres.body.each do |centre|
+  pp "#{centres.count} centres"
+
+  centres.each do |centre|
     add centre_path(centre.code), lastmod: centre.updated_at
     add centre_events_path(centre.code), priority: 0.5
     add centre_deals_path(centre.code), priority: 0.5
@@ -46,13 +43,13 @@ SitemapGenerator::Sitemap.create do
     add centre_info_path(centre.code), priority: 0.5
 
     # Stores
-    stores.body.find_all{|store| store.centre_id == centre.code }.each do |store|
+    Service::API.get(stores_uri, centre_id: centre.code).each do |store|
       add centre_store_path(centre.code, store.retailer_code, store.id), priority: 0.5, lastmod: store.updated_at
     end
   end
 
   product_responses.each do |pr|
-    pr.body.results.each do |product|
+    pr.results.each do |product|
       add product_path(product.retailer_code, product.sku), priority: 0.5, lastmod: product.updated_at
     end
   end

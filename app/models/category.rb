86673d5484e278cd50dd690c4971bb1e409b73b5
Malcolm Locke <malc.locke@trineo.co.nz>
2013-10-15 16:51:14 +1300

    Add super_cat parameter to T2 category links

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,14 +1,6 @@
 require 'hashie/mash'
 class Category < Hashie::Mash
-  def self_or_child_has_code?(code)
-    has_code?(code) || child_has_code?(code)
-  end
-
   def has_code?(code)
     code == self.code
-  end
-
-  def child_has_code?(code)
-    children.any? {|child| child.has_code?(code)}
   end
 end

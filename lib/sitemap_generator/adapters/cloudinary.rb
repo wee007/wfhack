8feba5db88cab50238f051506fe72572b7a61d3b
Ben Schwarz <ben@germanforblack.com>
2013-10-15 11:04:53 +1100

    Correct upload filename (as cloudinary does content negotiation automagically) Report the non-versioned url path of the final product

--- lib/sitemap_generator/adapters/cloudinary.rb
+++ lib/sitemap_generator/adapters/cloudinary.rb
@@ -7,8 +7,12 @@ module SitemapGenerator
       # Construct the path for cloudinary (it requires a rails root relative path)
       path = Pathname.new(location[:public_path] + location[:filename]).relative_path_from(Rails.root).to_s
 
-      file = Cloudinary::Uploader.upload(path, public_id: "sitemap.xml.gz", resource_type: :raw)
-      pp "Uploaded sitemap to #{file["url"]}"
+      file = Cloudinary::Uploader.upload(path, public_id: "sitemap.xml", resource_type: :raw)
+
+      # Construct the non-versioned url
+      public_path = file["url"].split("upload").first + "upload/" + file["public_id"]
+
+      pp "Uploaded sitemap to #{public_path}"
     end
   end
 end

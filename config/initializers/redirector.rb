35a5e350a754bd79027874accc205e3ad6dfd7cf
Adam Cohen <acohen@au.westfield.com>
2013-10-16 12:08:52 +1100

    WSF-4921 add initializer to instanciate global redirection vars

---
+++ config/initializers/redirector.rb
@@ -0,0 +1,15 @@
+  require 'csv'
+
+  def redirector_read_csv(filename)
+    categories = {}
+
+    CSV.foreach(filename) do |row|
+      categories[row[0].strip] = row[1].strip
+    end
+
+    categories
+  end
+
+REDIRECTOR_DELETED_CATEGORIES = redirector_read_csv("lib/assets/deleted_categories.csv")
+
+REDIRECTOR_CATEGORY_MAPPINGS = redirector_read_csv("lib/assets/categories.csv")

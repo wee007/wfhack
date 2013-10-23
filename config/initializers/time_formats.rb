fef49f647a130635d7e9387723e4ffb13e116f8b
Adam Cohen <acohen@au.westfield.com>
2013-10-23 21:46:29 +1100

    WSF-5811 fix broken specs

---
+++ config/initializers/time_formats.rb
@@ -0,0 +1,4 @@
+Time::DATE_FORMATS[:hour_minute_period] = "%l:%M%p"
+Time::DATE_FORMATS[:full_date] = '%-d %b %Y, %l:%M%p'
+Time::DATE_FORMATS[:short_date] = '%-d %b'
+Time::DATE_FORMATS[:short_date_with_year] = '%-d %b %Y'

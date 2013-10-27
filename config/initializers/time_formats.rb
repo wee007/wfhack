365aa364b777ddb20622e094cdb35e185ee236d6
Chris Pearce <CPearce@au.westfield.com>
2013-10-28 09:10:02 +1100

    Changing full date to lowercase pm in time formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,4 @@
 Time::DATE_FORMATS[:hour_minute_period] = "%l:%M%P"
-Time::DATE_FORMATS[:full_date] = '%-d %b %Y, %l:%M%p'
+Time::DATE_FORMATS[:full_date] = '%-d %b %Y, %l:%M%P'
 Time::DATE_FORMATS[:short_date] = '%-d %b'
 Time::DATE_FORMATS[:short_date_with_year] = '%-d %b %Y'

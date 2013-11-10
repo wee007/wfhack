f098c1b5943bc4ecec369b7fe516fa6826c6a910
Ben Schwarz <ben@germanforblack.com>
2013-11-11 08:08:34 +1100

    Remove non-used helpers. Add helper for returning the right path for whether there is a centre scoped or not.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,27 +1,11 @@
 module ProductsHelper
 
-  def products_sort_tag(sort_options)
-    default = ["","", {'data-sort-link' => centre_browse_path(params.reject{|k,v|k=='sort'})}]
-    values = sort_options.select{|s| s[:display]}.map do |s|
-      [s.description, s.code, {'data-sort-link' => centre_browse_path(params.merge sort: s.code)}]
-    end
-    selected = params[:sort]
-    select_tag(:sort, options_for_select([default]+values, selected), include_blank: false)
+  # This controller is either scoped by a centre or not,
+  # a simple helper to tidy the views
+  def pb_path
+    (@centre.nil?) ? browse_path : centre_browse_path(@centre)
   end
 
-  def price_tag(product)
-    if product.is_discounted
-      content_tag :p, class: 'price' do
-        was = content_tag(:del,number_to_currency(product.display_price))
-        now = content_tag(:span, number_to_currency(product.display_sale_price), class: 'sale')
-        "#{was} #{now}".html_safe
-      end
-    else
-      content_tag :p do
-        content_tag(:span, number_to_currency(product.display_price))
-      end
-    end
-  end
 
   def facet_remove_link(filter, value)
     url_params = params.dup

8f71fb328e11b9a62864615ccd039cbc29bc3db9
Ben Schwarz <ben@germanforblack.com>
2013-11-11 16:37:42 +1100

    Added canonical url helper

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -6,4 +6,38 @@ module ProductsHelper
     (@centre.nil?) ? products_path : centre_products_path(@centre)
   end
 
+  def canonical_url
+    centre_category or
+      centre_super_cat or
+      centre_products or
+      products_category or
+      products_super_cat or
+      products
+  end
+
+  private
+
+  def centre_category
+    centre_products_category_url(params[:centre], params[:super_cat], params[:category], params.except(:centre, :super_cat, :category)) if !params[:category].nil? and !params[:centre].nil?
+  end
+
+  def centre_super_cat
+    centre_products_super_cat_url(params[:centre], params[:super_cat], params.except(:centre, :category)) if !params[:super_cat].nil? and !params[:centre].nil?
+  end
+
+  def centre_products
+    centre_products_url(params[:centre], params.except(:centre)) if !params[:centre].nil?
+  end
+
+  def products_category
+    products_category_url(params[:super_cat], params[:category], params.except(:super_cat, :category)) if !params[:category].nil?
+  end
+
+  def products_super_cat
+    products_super_cat_url(params[:super_cat], params.except(:super_cat)) if !params[:super_cat].nil?
+  end
+
+  def products
+    products_url(params)
+  end
 end

60278ddda82aa7234c37da7be590e0a3fd55be9d
Ben Schwarz <ben@germanforblack.com>
2013-11-13 11:31:11 +1100

    Moved non-centre scoped route and added spec to cover

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -2,7 +2,15 @@ require 'spec_helper'
 
 describe CustomerConsole::Application do
   context "routing" do
-    it "routes click throughs" do
+    it "routes product click throughs" do
+      expect(:get => "/products/100/redirection").to route_to(
+        :controller => "products",
+        :action => "redirection",
+        :id => "100"
+      )
+    end
+
+    it "routes centre scoped product click throughs" do
       expect(:get => "/bondijunction/products/100/redirection").to route_to(
         :controller => "products",
         :action => "redirection",

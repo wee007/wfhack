8f6694e7f590568a3073c81d7540986cd3e9fcfa
Leon Dewey <ldewey@au.westfield.com>
2013-12-02 15:33:45 +1300

    Added the ablity to turn off VCR

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -3,6 +3,13 @@ require 'vcr'
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :faraday
-  c.default_cassette_options = { :record => :once, :allow_playback_repeats => true }
+  c.default_cassette_options = {
+    :record => :once,
+    :allow_playback_repeats => true
+  }
   c.configure_rspec_metadata!
+end
+
+unless ENV["RAILS_ENV"] == 'test'
+  VCR.turn_off! :ignore_cassettes => true
 end

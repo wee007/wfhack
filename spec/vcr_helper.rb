c51a90577579abf9effff279868a0732a187f864
Leon Dewey <ldewey@au.westfield.com>
2013-12-02 14:48:38 +1300

    Added basic smoke tests

--- spec/vcr_helper.rb
+++ spec/vcr_helper.rb
@@ -4,5 +4,5 @@ VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   c.hook_into :faraday
   c.default_cassette_options = { :record => :once, :allow_playback_repeats => true }
+  c.configure_rspec_metadata!
 end
-

7bd74b1f16f8f2f61ce0cdae85030c4e09b8e2e2
Fiona Chan <fiona@fionachan.net>
2013-12-16 15:50:22 +1100

    Unfocus search field when it's not visible

--- app/assets/javascripts/jquery-extensions/jquery.global-search-focus.js
+++ app/assets/javascripts/jquery-extensions/jquery.global-search-focus.js
@@ -1,8 +1,13 @@
 /* Focus in the global search input when it's opened via the toggle trigger */
 $(function() {
+  var isHeaderSearchActive = $('.js-header-search.is-active');
+  var headerSearchInput = $('.js-header-search input[type="search"]');
+
   $('.js-search-toggle').click(function() {
-    if($('.js-header-search.is-active').length) {
-      $('.js-header-search input[type="search"]').focus();
+    if(isHeaderSearchActive.length) {
+      headerSearchInput.focus();
+    } else {
+      headerSearchInput.blur();
     }
   });
 });

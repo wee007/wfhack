da33188677d4eaa783d40e64118d0a2b12979a01
alec <alec@smartalecwebsites.com.au>
2014-01-31 10:41:10 +1100

    start geolocation

---
+++ app/assets/javascripts/init/geolocation.js
@@ -0,0 +1,19 @@
+Geolocation =
+  rad: (x) ->
+    x * Math.PI / 180
+
+
+  # Distance in kilometers between two points using the Haversine algo.
+  haversine: (p1, p2) ->
+    R = 6371
+    dLat = @rad(p2.latitude - p1.latitude)
+    dLong = @rad(p2.longitude - p1.longitude)
+    a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(@rad(p1.latitude)) * Math.cos(@rad(p2.latitude)) * Math.sin(dLong / 2) * Math.sin(dLong / 2)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+    d = R * c
+    Math.round d
+
+
+  # Distance between me and the passed position.
+  distance_from: (current_location, to) ->
+    Geolocation.haversine current_location, to
---
+++ app/assets/javascripts/init/sort_centres.js.coffee
@@ -0,0 +1,2 @@
+#= require init/geolocation
+alert typeof Geolocation

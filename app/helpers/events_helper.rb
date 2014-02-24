bf56c8770139233ff1f16d01ffe0e61f818fb01b
Chris Pearce <cpearce@au.westfield.com>
2014-02-24 14:23:14 +1100

    Cleaning up Microdata - WSF-6198

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,13 +1,15 @@
 module EventsHelper
   def display_event_date(event)
-    content_tag(:time, :datetime => event.start_date(:raw)) do
-      if event.multiple_occurrences?
-        if event.multiple_occurrences_on_same_day?
+    if event.multiple_occurrences?
+      if event.multiple_occurrences_on_same_day?
+        content_tag(:time, datetime: event.start_date(:raw), itemprop: 'startDate') do
           event.start_date(:short_date_with_year)
-        else
-          "#{event.start_date(:short_date)} - #{event.end_date(:short_date)}"
         end
       else
+        content_tag(:time, event.start_date(:short_date), datetime: event.start_date(:raw), itemprop: 'startDate') + " &#8211; ".html_safe + content_tag(:time, event.end_date(:short_date), datetime: event.end_date(:raw), itemprop: 'endDate')
+      end
+    else
+      content_tag(:time, datetime: event.start_date(:raw), itemprop: 'startDate') do
         event.start_date(:full_date)
       end
     end

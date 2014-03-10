c67778be21a39ba84c19d78837869e347c928837
Chris Pearce <cpearce@au.westfield.com>
2014-03-10 15:59:44 +1100

    Defining the master grid dimensions and start of pinboard fixed height styling

--- app/helpers/curations_helper.rb
+++ app/helpers/curations_helper.rb
@@ -1,21 +1,25 @@
 module CurationsHelper
 
-  NumberOfProducts = 10
+  NumberOfProducts = 30
 
   ProductCloudinaryUrls = %w[
-    http://res.cloudinary.com/wlabs/image/fetch/c_lpad,f_auto,h_474,w_469/http://res.cloudinary.com/wlabs/image/upload/v1384479265/lpyfmlkrpvs6yuedylxw.jpg
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1384479265/lpyfmlkrpvs6yuedylxw.jpg
 
-    http://res.cloudinary.com/wlabs/image/fetch/c_lpad,f_auto,h_474,w_469/http://res.cloudinary.com/wlabs/image/upload/v1393381198/adhzebpx2ydbnelp3uuu.jpg
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1393381198/adhzebpx2ydbnelp3uuu.jpg
 
-    http://res.cloudinary.com/wlabs/image/fetch/c_lpad,f_auto,h_474,w_469/http://res.cloudinary.com/wlabs/image/upload/v1389193935/vi5p54hlo652kqrgtnew.jpg
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1389193935/vi5p54hlo652kqrgtnew.jpg
 
-    http://res.cloudinary.com/wlabs/image/fetch/c_lpad,f_auto,h_474,w_469/http://res.cloudinary.com/wlabs/image/upload/v1381316112/oxtlmtu5osxdnati57wi.jpg
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1381316112/oxtlmtu5osxdnati57wi.jpg
 
-    http://res.cloudinary.com/wlabs/image/fetch/c_limit,f_auto,w_289/http://res.cloudinary.com/wlabs/image/upload/v1393290790/jgp1zlrmdihr1ujsamai.jpg
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1393290790/jgp1zlrmdihr1ujsamai.jpg
 
-    http://res.cloudinary.com/wlabs/image/fetch/c_lpad,f_auto,w_289/http://res.cloudinary.com/wlabs/image/upload/v1381336879/l8itsnowlxzljr7vsd6d.jpg
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1381336879/l8itsnowlxzljr7vsd6d.jpg
 
-    http://res.cloudinary.com/wlabs/image/fetch/c_lpad,f_auto,h_474,w_469/http://res.cloudinary.com/wlabs/image/upload/v1393467025/yxqkvz9e9fionmsxk287.jpg
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1393467025/yxqkvz9e9fionmsxk287.jpg
+
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1386718425/tvvuer6s6mq9xygcgmbe.jpg
+
+    http://res.cloudinary.com/wlabs/image/fetch/c_pad,f_auto,w_300,h_242/http://res.cloudinary.com/wlabs/image/upload/v1394210320/pwaqtftmrbpnprk1qvwb.jpg
   ]
 
   def curated_products

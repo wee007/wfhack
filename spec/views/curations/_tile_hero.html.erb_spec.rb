186ddd4f2246f4a32b8809e706534da8aa7fb24e
Michael Bamford <michael@michaelbamford.com>
2014-03-19 10:55:37 +1100

    Removed obsolete curations/_tile_hero, _tile_sidekick partials

--- spec/views/curations/_tile_hero.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'curations/_tile_hero.html.erb' do
-  let(:image_url) { 'abc.jpg' }
-  let(:tile_hero) {{ id: 666, image_url: image_url }}
-  before { view.stub(:tile_hero) { tile_hero }}
-  subject { render; rendered }
-  it { should match %r{#{image_url}} }
-end
--- spec/views/curations/_tile_sidekick.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'curations/_tile_sidekick.html.erb' do
-  let(:image_url) { 'abc.jpg' }
-  let(:tile_sidekick) {{ id: 666, image_url: image_url }}
-  before { view.stub(:tile_sidekick) { tile_sidekick }}
-  subject { render; rendered }
-  it { should match %r{#{image_url}} }
-end

707d15d9f736f4340f9eeafc7a790464d99ea2a7
Justin Lambert <jlambert@au.westfield.com>
2014-03-26 16:31:23 +1100

    WSF-6243 added redirection logic in case we have exactly one hardcoded result

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,6 +9,15 @@ class SearchController < ApplicationController
 
     gon.centre = params[:centre_id]
 
+    # See WSF-6243; if we have only one hardcoded result, go straight to the result.
+    if (@search.results.count == 1)
+      # I'm sure there's a nicer way to access the result info; I'd love to hear about it in the code review :)
+      sole_result = @search.results.first
+      if sole_result.first == "centre_information"
+        redirect_to sole_result.second.first["attributes"]["path"]
+      end
+    end
+
     # TODO: sort by search model
     @search.results = @search.results.sort.reverse
 

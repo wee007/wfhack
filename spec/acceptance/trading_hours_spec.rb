6d50489e6b87d863b5bca07a21d1cdf455d3289a
Ben Pritchard <benpritchard@aud27140.au.ad.westfield.com>
2014-04-09 13:37:23 +1000

    changes due to code review

--- spec/acceptance/trading_hours_spec.rb
+++ spec/acceptance/trading_hours_spec.rb
@@ -4,7 +4,7 @@ require 'trading_hours_helper'
 feature 'Centre has correct trading Hours', :destructive => false do
   scenario "Bondi Junction has correct trading hours" do
     visit "/bondijunction/hours"
-    hours_table = find('#tab-0 table', :text => 'Centre Hours')
+    hours_table = find('.test-centre-hours-table-0')
     this_weeks_centre_hours.each do |hour|
       expect(hours_table).to have_text(expected_hours_string(hour))
     end
@@ -14,7 +14,7 @@ end
 feature 'Store has correct trading Hours', :destructive => false do
   scenario "David Jones has correct trading hours" do
     visit "/bondijunction/hours?store_id=4565#shopping-hours"
-    hours_table = find('#tab-0 #shopping-hours table')
+    hours_table = find('.test-store-hours-table-0')
     this_weeks_store_hours.each do |hour|
       expect(hours_table).to have_text(expected_hours_string(hour))
     end

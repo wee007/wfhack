44a592eac7eeffe224027a1ff4ef9a0ffaa60869
Ben Pritchard <benpritchard@aud27140.au.ad.westfield.com>
2014-04-30 12:33:04 +1000

    removed sample test

--- spec/acceptance/smoke_spec.rb
+++ spec/acceptance/smoke_spec.rb
@@ -11,19 +11,5 @@ feature 'Smoke', :smoke => true do
       visit '/status.json'
       expect(page).to have_content '"healthy":true','"message":"success"'
     end
-    
-    scenario 'edit the user\'s profile' do
-      destructive 'submit the form' do
-        visit '/status.json'
-        expect(page).to have_content '"healthy":true','"message":"success"'
-      end
-    end
-    
-    scenario 'delete the database' do
-      destructive do
-        visit '/status.json'
-        expect(page).to have_content '"healthy":true','"message":"success"'
-      end
-    end
   end
 end

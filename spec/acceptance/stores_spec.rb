0baba3a4eb921c86b81214a5d67449bbaa74ea2d
alec <alec@smartalecwebsites.com.au>
2014-05-05 11:53:14 +1000

    simplify test selectors

--- spec/acceptance/stores_spec.rb
+++ spec/acceptance/stores_spec.rb
@@ -5,14 +5,14 @@ feature 'Stores' do
   describe 'There are stores in the store list' do
     scenario "for Sydney" do
       visit "/sydney/stores"
-      expect(page).to have_css '.test-stores-store-link'
+      expect(page).to have_css '.test-store-link'
     end
   end
 
   describe 'View Storefront' do
     scenario "from Sydney stores list" do
       visit "/sydney/stores"
-      store_link = random('.test-stores-store-link')
+      store_link = random('.test-store-link')
       store_link.click
       expect(page).to have_css '.test-stores-show'
     end

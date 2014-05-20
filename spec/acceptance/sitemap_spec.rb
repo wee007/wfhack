c1630af9dbfac99af9d46c20d4bbc247253deac5
Charles Horn <chorn@au.westfield.com>
2014-05-21 10:41:10 +1200

    test for sitemap update time and content

--- spec/acceptance/sitemap_spec.rb
+++ spec/acceptance/sitemap_spec.rb
@@ -19,7 +19,16 @@ feature 'Sitemaps and robots.txt' do
   if environment_greater_than_or_equal_to? 'production' 
     describe 'Sitemaps' do
       scenario 'The sitemap should have been updated within the last 24 hours and contain multiple sub-sitemaps' do
-        pending
+        visit '/sitemap.xml.gz'
+        gz = Zlib::GzipReader.new(StringIO.new(page.body))
+        inflated = gz.read
+        # currently (May 2014) we should have 4 split sitemaps
+        expect(inflated).to have_xpath '//sitemap', :minimum => 4
+        # each of our sub-sitemaps should have been created within the last day
+        inflated.scan(/<lastmod>(.*?)<\/lastmod>/).each do |d|
+          date = Date.parse(d[0])
+          expect(date).to be_within(1).of(Date.today)
+        end
       end
     end
   end

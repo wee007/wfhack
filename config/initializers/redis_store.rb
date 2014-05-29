009ea7e5abec164f5dd1d1fb8fbc06731c73d087
Peter McInerney <pmcinerney@au.westfield.com>
2014-05-29 20:13:39 +1000

    heroku fix

--- config/initializers/redis_store.rb
+++ config/initializers/redis_store.rb
@@ -1,4 +1,4 @@
 redis_url = ENV["REDISCLOUD_URL"] || "redis://127.0.0.1:6379/0/myapp"
-MyApp::Application.config.cache_store = :redis_store, redis_url
-MyApp::Application.config.session_store :redis_store, redis_server: redis_url
+CustomerConsole::Application.config.cache_store = :redis_store, redis_url
+CustomerConsole::Application.config.session_store :redis_store, redis_server: redis_url
 
